8f0fbe3b4f1ca276f0d649d4e9fa2a9d67e8dc18
Mario Guaqueta <marioguaqueta@Marios-MacBook-Pro.local>
2020-02-11 10:57:46 -0500

    development

--- server/index.js
+++ server/index.js
@@ -21,6 +21,71 @@ app.use(bodyParser.raw({
 app.use(express.static(path.join(__dirname, '../public')));
 
 
+app.post('/save',function (req, res){
+  console.log('ON SAVE');
+  console.log(req.headers);
+  console.log(req.body);
+
+  res.status(200);
+  res.send({
+      route: 'save'
+  });
+});
+
+app.post('/publish',function (req, res){
+  console.log('ON PUBLISH');
+  console.log(req.headers);
+  console.log(req.body);
+  res.status(200);
+  res.send({
+      route: 'publish'
+  });
+});
+
+
+
+app.post('/validate',function (req, res){
+  console.log('ON VALIDATE');
+  console.log(req.headers);
+  console.log(req.body);
+  res.status(200);
+  res.send({
+      route: 'validate'
+  });
+});
+
+
+app.post('/execute',function (req, res){
+
+  console.log('EXECUTE');
+
+  JWT(req.body, Pkg.options.salesforce.marketingCloud.jwtSecret, (err, decoded) => {
+      
+      if (err) {
+          console.log("ERR: " + err);
+          return res.status(401).end();
+      }
+
+      if (decoded && decoded.inArguments && decoded.inArguments.length > 0) {
+
+          //Logica de backend
+          res.status(200);
+          res.send({
+              route: 'execute'
+          });
+      } else {
+          console.error('inArguments invalid.');
+          return res.status(400).end();
+      }
+
+
+
+  });
+
+
+});
+
+
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
