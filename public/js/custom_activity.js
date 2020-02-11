3c7c42c5897c04120e268b9cd6d710a1f9a5ba5a
Mario Guaqueta <marioguaqueta@Marios-MacBook-Pro.local>
2020-02-11 11:54:07 -0500

    development

--- public/js/custom_activity.js
+++ public/js/custom_activity.js
@@ -236,12 +236,12 @@ define(['postmonger'], (Postmonger) => {
                 }
             }
         }
-        $(select01).change(validateOnChange());
-        $(select02).change(validateOnChange());
-        $(select03).change(validateOnChange());
-        $(select04).change(validateOnChange());
-        $(select05).change(validateOnChange());
-        $(select06).change(validateOnChange());
+        $(select01).change(validateOnChange);
+        $(select02).change(validateOnChange);
+        $(select03).change(validateOnChange);
+        $(select04).change(validateOnChange);
+        $(select05).change(validateOnChange);
+        $(select06).change(validateOnChange);
     }
 
     function validateOnChange(){
